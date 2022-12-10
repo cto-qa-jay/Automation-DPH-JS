12850a22dbbf8b05d787254bc1fe396db6bb3b67
cto-qa-jay <jay@ctoglobal.co>
2022-12-09 20:20:11 -0500

    feat: added 2 folder, and added 3 files & locators

--- pageobjects/My Posts Page/Add New Post Page/AddNewPostPage.js
+++ pageobjects/My Posts Page/Add New Post Page/AddNewPostPage.js
@@ -5,24 +5,24 @@ class AddNewPostPage extends MyPostPage {
     super(page);
     this.onDemandButton = page.locator('.onDemand-button');
     this.scheduledDeliveryButton = page.locator('.scheduled-button');
-    this.referenceNumberField = page.locator('');
-    this.senderNameField = page.locator('');
-    this.mobileNumberField = page.locator('');
-    this.emailAddressField = page.locator('');
-    this.addressField = page.locator('');
-    this.provinceFieldAndDropDown = page.locator('');
-    this.cityFieldAndDropDown = page.locator('');
-    this.barangayFieldAndDropDown = page.locator('');
-    this.postalCodeField = page.locator('');
-    this.pickUpDateAndTimeCalendar = page.locator('');
-    this.hubDropDown = page.locator('');
-    this.remarksField = page.locator('');
-    this.tagsFieldAndDropDown = page.locator('');
+    this.referenceNumberField = page.locator('[name=refNo]');
+    this.senderNameField = page.locator('[name=customerName]');
+    this.mobileNumberField = page.locator('[name=contactNumber]');
+    this.emailAddressField = page.locator('[name=emailAddress]');
+    this.addressField = page.locator('[name=address]');
+    this.provinceFieldAndDropDown = page.locator('.css-1hwfws3 >> nth=0');
+    this.cityFieldAndDropDown = page.locator('.css-1hwfws3 >> nth=1');
+    this.barangayFieldAndDropDown = page.locator('.css-1hwfws3 >> nth=2');
+    this.postalCodeField = page.locator('[name=postalCode]');
+    this.pickUpDateAndTimeCalendar = page.locator('.rdt');
+    this.hubDropDown = page.locator('.css-1hwfws3 >> nth=3');
+    this.remarksField = page.locator('[name=remarks]');
+    this.tagsFieldAndDropDown = page.locator('.css-1hwfws3 >> nth=4');
 
-    this.autoFillPickUpDetailsButton = page.locator('');
-    this.userLastProfileButton = page.locator('');
+    this.autoFillPickUpDetailsButton = page.locator('text=Auto-fill Pick-up Details');
+    this.userLastProfileButton = page.locator('text=Use Last Profile');
 
-    this.previousStepButton = page.locator('');
+    this.previousStepButton = page.locator('text=Previous Step');
     this.nextStepButton = page.locator('text=Next Step');
   }
 }
